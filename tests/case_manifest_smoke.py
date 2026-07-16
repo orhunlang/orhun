3af78315021629e4c32c91e7b8a114d3e64caa64
Orhun Language <orhunlang@gmail.com>
2026-07-16 21:27:56 +0300

    fix: register module cache fixtures

--- tests/case_manifest_smoke.py
+++ tests/case_manifest_smoke.py
@@ -6,6 +6,8 @@ EXPECTEDLESS_CASES = {
     "math_features",
     "mod_in_func",
     "module_callable_lib",
+    "module_cycle_a_lib",
+    "module_cycle_b_lib",
     "primes_loop_caller",
     "primes_loop_once",
     "primes_mini",
